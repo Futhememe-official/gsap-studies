0aa925f1fb5f1cba784b556ae142ec2d977f52d9
Gustavo Souza <kyoro_souza@hotmail.com>
2026-03-10 22:16:51 -0300

    🎨 create animations

---
+++ src/features/study-04-menu/view.model.ts
@@ -0,0 +1,55 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef, useState } from "react";
+
+gsap.registerPlugin(useGSAP);
+
+export const useStudy4Menu = () => {
+  const leftImageRef = useRef(null);
+  const rightImageRef = useRef(null);
+  const container = useRef(null);
+  const { contextSafe } = useGSAP({ scope: container });
+
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const onHoverLink = contextSafe(
+    ({ left, right }: { left: number; right: number }) => {
+      gsap.to(leftImageRef.current, { y: left });
+      gsap.to(rightImageRef.current, { y: right });
+    },
+  );
+
+  const openMenuAnimation = contextSafe(() => {
+    const tl = gsap.timeline();
+    tl.to(container.current, { opacity: 1 }).to(container.current, {
+      y: 0,
+      delay: -0.5,
+    });
+  });
+
+  const handleMenuClose = contextSafe(() => {
+    const tl = gsap.timeline();
+    tl.to(container.current, {
+      y: "-100%",
+      ease: "power1.inOut",
+    }).to(container.current, {
+      opacity: 0,
+      onComplete: () => setIsMenuOpen(false),
+    });
+  });
+
+  const handleMenuOpen = () => {
+    setIsMenuOpen(true);
+    openMenuAnimation();
+  };
+
+  return {
+    onHoverLink,
+    container,
+    leftImageRef,
+    rightImageRef,
+    handleMenuOpen,
+    handleMenuClose,
+    isMenuOpen,
+  };
+};
--- src/features/study-04-menu/view.tsx
+++ src/features/study-04-menu/view.tsx
@@ -2,8 +2,19 @@ import { PageLayout } from "@/shared/components/PageLayout";
 // @ts-ignore
 import styles from "./menu-view.module.css";
 import { Menu } from "lucide-react";
+import { useStudy4Menu } from "./view.model";
 
 export function Study4Menu() {
+  const {
+    container,
+    leftImageRef,
+    rightImageRef,
+    onHoverLink,
+    isMenuOpen,
+    handleMenuOpen,
+    handleMenuClose,
+  } = useStudy4Menu();
+
   return (
     <PageLayout
       title="Menu"
@@ -15,10 +26,63 @@ export function Study4Menu() {
       <header className={`${styles.header}`}>
         <div />
         <p>WithGu</p>
-        <button>
+        <button
+          onClick={() => {
+            if (isMenuOpen) {
+              handleMenuClose();
+            } else {
+              handleMenuOpen();
+            }
+          }}
+        >
           <Menu color="#fff" size={24} />
         </button>
       </header>
+      <nav className={`${styles.nav}`} ref={container}>
+        <div className={`${styles.navContent}`}>
+          <div className={`${styles.leftImageHolder}`} ref={leftImageRef} />
+          <div className={`${styles.links}`}>
+            <h2
+              onMouseEnter={() => {
+                onHoverLink({ left: 25, right: -25 });
+              }}
+              onMouseLeave={() => {
+                onHoverLink({ left: 0, right: 0 });
+              }}
+            >
+              Portfolio
+            </h2>
+            <h2
+              onMouseEnter={() => {
+                onHoverLink({ left: 0, right: 0 });
+              }}
+              onMouseLeave={() => {
+                onHoverLink({ left: 0, right: 0 });
+              }}
+            >
+              About
+            </h2>
+            <h2
+              onMouseEnter={() => {
+                onHoverLink({ left: -25, right: 25 });
+              }}
+              onMouseLeave={() => {
+                onHoverLink({ left: 0, right: 0 });
+              }}
+            >
+              Contact
+            </h2>
+          </div>
+          <div className={`${styles.rightImageHolder}`} ref={rightImageRef} />
+        </div>
+        <footer className={`${styles.navFooter}`}>
+          <div>
+            <a href="#">Instagram</a>
+            <a href="#">Github</a>
+          </div>
+          <button>call to action</button>
+        </footer>
+      </nav>
     </PageLayout>
   );
 }
